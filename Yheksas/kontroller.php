9e142e21d219e4d44235d02f0599d3d981e07687
koidermaa <oidermaa@gmail.com>
2016-04-13 20:42:38 +0300

    üheksas kodutöö

---
+++ Yheksas/kontroller.php
@@ -0,0 +1,37 @@
+﻿<?php 
+$pildid=array(
+  array("source"=>"pildid/nameless1.jpg", "alt"=>"nimetu 1", "id"=>1),
+  array("source"=>"pildid/nameless2.jpg", "alt"=>"nimetu 2", "id"=>2),
+  array("source"=>"pildid/nameless3.jpg", "alt"=>"nimetu 3", "id"=>3),
+  array("source"=>"pildid/nameless4.jpg", "alt"=>"nimetu 4", "id"=>4),
+  array("source"=>"pildid/nameless5.jpg", "alt"=>"nimetu 5", "id"=>5),
+  array("source"=>"pildid/nameless6.jpg", "alt"=>"nimetu 6", "id"=>6)	
+ );
+
+ 
+$mode="pealeht"; 		
+	if (isset($_GET["mode"]) && $_GET["mode"]!="") {
+   	 $mode=htmlspecialchars($_GET["mode"]); 
+	}
+include_once "head.html";
+	
+switch ($mode) {
+    case "pealeht":
+		include "pealeht.html";
+        break;
+    case "galerii":
+		include "galerii.html";
+        break;
+    case "vote":
+		include "vote.html";
+        break;
+    case "tulemus":
+		include "tulemus.html";
+        break;
+    default:
+		include "pealeht.html";
+        break;
+}	
+include_once "foot.html";	
+ 	
+?>
