f81771ea83e17bd56dfea1870a2a8a3474e42d04
koidermaa <oidermaa@gmail.com>
2016-04-18 23:34:40 +0300

    kymnes kodutöö

---
+++ Kümnes/lopp.php
@@ -0,0 +1,14 @@
+<?php
+
+if(isset($_COOKIE[session_name()])) {
+setcookie(session_name(), '',
+time()-42000, '/');
+}
+
+$_SESSION = array();
+
+session_destroy();
+
+header("Location: kontroller.php?mode=vote");
+
+?>
